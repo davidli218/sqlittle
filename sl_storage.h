12ffcb38ae932585552e5fe4180a358dee27ff71
RI TEII <teiiri@outlook.com>
2022-01-21 22:02:35 +0000

    Change the maximum length of E-Mail address to 128Bytes

--- sl_storage.h
+++ sl_storage.h
@@ -13,7 +13,7 @@
 
 #define TABLE_MAX_PAGES 100
 #define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
+#define COLUMN_EMAIL_SIZE 128
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
